eeed3d80ae8c33083e93c49beb003186fa2a9305
DmooSoCool <dmoosocool@gmail.com>
2018-03-23 01:19:57 +0800

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div v-show="!loaded">加载中...</div>
+    <div v-show="loaded" class="main">
+      <span>{{text}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  props: {
+  },
+  data() {
+    return {
+      text: '',
+      loaded: false,
+    };
+  },
+
+  beforeCreate() {
+    setTimeout(() => {
+    }, 1000);
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.text = '加载完成';
+      this.loaded = true;
+    }, 2000);
+  },
+};
+</script>
+
+<style lang="stylus">
+
+</style>
